2fe18826d9384d7570424c951a14ac1a04ef9b7c
xuewenyu <3515281812@qq.com>
2023-02-27 17:07:58 +0800

    session练习

--- book/views.py
+++ book/views.py
@@ -51,3 +51,34 @@ http://ip:port/path/path?key1=value1&key2=value2...
 request.GET  获取查询字符串的信息  返回 <QueryDict {"key1"={"value1"}}>
 
 """
+
+
+
+##################session#######################
+def set_session(request):
+
+    user_id = 1
+    username = request.GET.get('username')
+    print('username', username)
+    request.session['user_id'] = user_id
+    request.session["username"] = username
+
+    ####删除session数据#####
+    # clear 删除session里的数据，保留key
+    # request.session.clear()
+    # flush 删除session的 键和值 的数据
+    # request.session.flush()
+
+    # 设置session的有效期 单位：秒
+    request.session.set_expiry(60*60*24*14)
+
+    return HttpResponse('set_session')
+
+
+def get_session(request):
+
+    user_id = request.session.get('user_id')
+    username = request.session.get('username')
+    content = '{}, {}'.format(user_id, username)
+    print('==========', request.COOKIES)
+    return HttpResponse(content)
